1a57278745ba8b16711dff22e164e22cfd8761b9
Abdelfattah225 <abdelfattahelazazy@gmail.com>
2026-01-19 15:20:20 +0200

    build: add Bazel configuration for Phase 2 telemetry system

--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abdo/ITI/03-Modern_CPP/ITI_Cpp_LogLog_Project/ITI_Cpp_LogLog_Project/build/_deps/googletest-src/googletest
-# Build directory: /home/abdo/ITI/03-Modern_CPP/ITI_Cpp_LogLog_Project/ITI_Cpp_LogLog_Project/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
